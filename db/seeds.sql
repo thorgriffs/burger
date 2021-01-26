bf0bf785e6dfc66f6de003870cad6b2c1974b799
thorgriffs <22.kelliking@gmail.com>
2021-01-25 20:28:02 -0500

    modified seeds.sql insert data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder with Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Beyond Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Whopper');
 INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Burger');
-
 INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
